19a2ad2de2ca4d3ae80476e0e26f57d118826c3b
Junjun Jiang <junjun0595@163.com>
2020-05-18 23:37:02 +0800

    Create demo.sh

---
+++ demo.sh
@@ -0,0 +1,4 @@
+python mains.py train --dataset_name 'Chikusei' --n_blocks 3 --epochs 40 --batch_size 32 --n_subs 8 --n_ovls 2 --n_feats 256 --n_scale 4 --gpus "0,1"
+python mains.py train --dataset_name 'Chikusei' --n_blocks 3 --epochs 40 --batch_size 32 --n_subs 8 --n_ovls 2 --n_feats 256 --n_scale 4 --gpus "0,1"
+python mains.py train --dataset_name 'Cave' --n_blocks 8 --epochs 40 --batch_size 16 --n_subs 4 --n_ovls 1 --n_feats 256 --n_scale 4 --gpus "0,1"
+python mains.py train --dataset_name 'Cave' --n_blocks 8 --epochs 40 --batch_size 16 --n_subs 4 --n_ovls 1 --n_feats 256 --n_scale 8 --gpus "0,1"
